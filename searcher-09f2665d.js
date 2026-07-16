4970215acb3aa1d1b5464054a36fb988d5dc99bd
Ravan Naidoo <ravan@mac.home>
2026-07-17 00:45:09 +0200

    docs: publish ReBAC chapter

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b7ef6f64.js',
+                path_to_root + 'searchindex-413e4ee5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
